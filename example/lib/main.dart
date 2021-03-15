ff80de315d3d2c99a8b411ce489f611b2421706c
Chiziaruhoma <ogbondachiziaruhoma@gmail.com>
2021-03-15 07:16:54 +0100

    migrated to null saftey

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_mono/flutter_mono.dart';
 
 void main() {
@@ -10,36 +11,34 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.white,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      child: MaterialApp(
+        title: 'Mono Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: MyHomePage(title: 'Mono Demo'),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key? key, this.title}) : super(key: key);
 
-  final String title;
+  final String? title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String code = '';
+  String? code = '';
 
   final monoPublicKey = '';
 
@@ -47,39 +46,69 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(
+          widget.title ?? '',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+          ),
+        ),
+        centerTitle: true,
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
       ),
+      backgroundColor: Colors.white,
       body: Center(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(code ?? ''),
-          const SizedBox(
-            height: 150,
-          ),
-          Container(
-            height: 50,
-            width: 200,
-            child: RaisedButton(
-              color: Colors.blue,
-              textColor: Colors.white,
-              child: Text('Launch Mono'),
-              onPressed: () async {
-                await MonoFlutter.launchMono(
-                  context,
-                  key: monoPublicKey,
-                  onClosed: () {},
-                  onSuccess: (String _code) {
-                    setState(() {
-                      code = _code;
-                    });
-                  },
-                );
-              },
+          SelectableText(
+            code ?? '',
+            style: TextStyle(
+              color: monoColor,
+              fontWeight: FontWeight.w600,
+              fontSize: 25,
             ),
+          ),
+          const SizedBox(height: 60),
+          Column(
+            children: [
+              Container(
+                height: 50,
+                width: 260,
+                child: CupertinoButton(
+                  color: monoColor,
+                  child: Center(
+                    child: Text(
+                      'Launch Mono',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  onPressed: () async {
+                    await MonoFlutter.launchMono(
+                      context,
+                      key: monoPublicKey,
+                      onClosed: () {
+                        Navigator.pop(context);
+                      },
+                      onSuccess: (String _code) {
+                        setState(() {
+                          code = _code;
+                        });
+                        Navigator.pop(context);
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ],
       )),
     );
   }
 }
+
+final monoColor = Color(0xff182cd1);
